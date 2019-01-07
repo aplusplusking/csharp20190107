7410da4e449e76f0ffefe6f707928bc4f555f225
nezumi <khc_2007@naver.com>
2019-01-07 13:29:31 +0900

    size

---
+++ TypeCastExample1/TypeCastExample1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TypeCastExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(default(int));
+            int iSize = sizeof(int);
+            Console.WriteLine($"int형 바이트길이 : {iSize}");
+
+            Type myType1 = typeof(int);
+            Console.WriteLine($"type(int) : {myType1}");
+
+            int i = 10;
+            Type myType2 = i.GetType();
+            Console.WriteLine($"i.GetType() : {myType2}");
+
+            int j = 99;
+            Console.WriteLine("Size of j : {0}", Marshal.SizeOf(j));
+        }
+    }
+}
