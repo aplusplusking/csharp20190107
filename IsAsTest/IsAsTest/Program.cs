8534c284eaaac58461390c82e6ac43f43f290d31
nezumi <khc_2007@naver.com>
2019-01-07 13:39:18 +0900

    as Emp

---
+++ IsAsTest/IsAsTest/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AsTest
+{
+    class Emp
+    {
+        public override string
+       ToString()
+        {
+            return "Emp";
+        }
+    }
+    class Programmer : Emp { } //자식클래<<상속<<부모클래스
+    class Program
+    {
+        static void Main()
+        {
+            Programmer p = new Programmer();
+            if (p is Emp)//참
+            {
+                System.Console.WriteLine("p is Emp");
+            }
+            Emp e =(Emp)p;
+            if (e != null)
+                System.Console.WriteLine(e.ToString());
+        }
+    }
+}
