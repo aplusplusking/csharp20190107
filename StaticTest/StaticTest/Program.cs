8d4aa2770b76d18a3147561f7a41d24e862c7b65
nezumi <khc_2007@naver.com>
2019-01-07 12:06:56 +0900

    Main

---
+++ StaticTest/StaticTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StaticTest{
+    class Emp
+    {
+       public string name;//1
+       public static string compName;//2
+       public void GoToOffice()//1
+        {
+            Console.WriteLine($"{compName} 소속 {name}(이)가 출근합니다.");
+        }
+    }
+    class EmpTest
+    {
+        static void Main(string[] args)
+        {
+            Emp.compName = "탑크리에듀";//2
+            Emp e = new Emp();//1
+            e.name = "홍길동"; e.GoToOffice();//1
+        }
+    }
+}
