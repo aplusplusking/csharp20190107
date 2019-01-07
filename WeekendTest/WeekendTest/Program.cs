c0e145c834acf97404d2f65b833b88ea2178ceef
nezumi <khc_2007@naver.com>
2019-01-07 14:40:22 +0900

    호도독

---
+++ WeekendTest/WeekendTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WeekendTest
+{
+    enum Day1
+    {
+        Monday, Thesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    }
+    enum Day2 : byte
+    {
+        Monday = 11, Thesday, Wednesday, Thursday, Friday, Saturday, Sunday = Monday + 100
+    }
+    public class EnumTest
+    {
+        static void Main(string[] args)
+        {
+            Day1 whatDay = Day1.Sunday;
+            Console.WriteLine($"{whatDay}");
+            Console.WriteLine($"{(int)whatDay}");
+
+            whatDay = (Day1)6;
+            Console.WriteLine($"{whatDay}");
+
+            Day2 whatDay2 = Day2.Monday;
+            Console.WriteLine($"{whatDay2}");
+            Console.WriteLine($"{(byte)whatDay2}");
+
+            whatDay2 = (Day2)111;
+            Console.WriteLine($"{whatDay2}");
+        
+        }
+    }
+}
