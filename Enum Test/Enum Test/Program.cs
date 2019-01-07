795030fdbf0b5c0f4ff0fcbff9299bb997fbfb17
nezumi <khc_2007@naver.com>
2019-01-07 18:07:27 +0900

    7까지 숫자

---
+++ Enum Test/Enum Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ParamsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum());
+            Console.WriteLine(Sum());
+            Console.WriteLine(Sum(-1, -2, -3, -4, -5));
+            Console.WriteLine(Sum(new int[] { 1, 2, 3 }));
+            Console.WriteLine(Sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15));
+            Console.WriteLine(Sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+            Console.WriteLine(Sum(1, 2, 3, 4, 5));
+            Console.WriteLine(Sum(new int[] { 1, 2, 3 }));
+        }
+        static int Sum(params int[] iArr) 
+        {
+            int sum = 0;
+            foreach(int i in iArr)
+            {
+                sum += i;
+            }
+            return sum;
+
+           
+
+        }
+    }
+}
---
+++ Test/Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SevenCountTest
+{
+    class SevenCount
+    {
+
+        static int n;
+        
+
+
+        static int count;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("일정한 범위 내에서 7이 얼마나 있나 알아보는 프로그램입니다.\n숫자를 입력하세요:");
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int i=1; i<=n; i++)
+            {
+                searchSeven(i);
+            }
+            Console.WriteLine($"{count}");
+        }
+        public static void searchSeven(int num)
+        {
+            if (num % 10 == 7) count++;
+            if (num > 10) searchSeven(num / 10);
+        }
+    }
+
+}
