fd471b139d3d8425aa8731425b808be5636705dd
nezumi <khc_2007@naver.com>
2019-01-08 17:30:26 +0900

    a

---
+++ ReadLineFromFile/ReadLineFromFile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReadLineFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = Sum(@"c:\GitHub\number");
+            Console.WriteLine($"{sum}");
+
+        }
+        static int Sum(string file)
+        {
+            int sum = 0;
+            string[] arr = File.ReadAllLines(file);
+            foreach (string s in arr) sum += Int32.Parse(s);
+            return sum;
+        }
+    }
+}
