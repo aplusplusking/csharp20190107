1ea540ce03968b5712b347b28d6768ee37a0d773
nezumi <khc_2007@naver.com>
2019-01-08 18:02:00 +0900

    ff

---
+++ CreateDir/CreateDir/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Emp
+{
+
+   public  override string ToString()
+    {
+        return "This is Emp class";
+    }
+
+}
+
+public class ToStringMethod
+
+{
+
+    static void Main()
+
+    {
+
+        Emp e = new Emp();
+
+        Object o = new Object();
+
+
+
+        Console.WriteLine(o.ToString());
+
+        Console.WriteLine(e.ToString());
+
+        Console.WriteLine(e);
+
+    }
+
+}
