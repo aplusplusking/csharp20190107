13a5069f513c9a39134bb529b368fa614e812fbf
nezumi <khc_2007@naver.com>
2019-01-08 10:00:24 +0900

    자동구현 속성

---
+++ Day2/Day2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Day2
+{
+    class Emp:Object
+    {
+        
+        public string Name
+        {
+            get; set;         
+        }
+        public override string ToString()
+        {
+            return $"[사원]{Name}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Emp e = new Emp();
+            e.Name="홍";
+            Console.WriteLine(e.Name);
+            
+        }
+    }
+}
--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -15,8 +15,19 @@ namespace SevenCountTest
         {
 
             Console.WriteLine("일정한 범위 내에서 7이 얼마나 있나 알아보는 프로그램입니다.\n숫자를 입력하세요:");
+            while (true)
+            {
+                bool parsed = Int32.TryParse(Console.ReadLine(), out n);
+                if (!parsed)
+                {
+                    Console.WriteLine("숫자가 범위값을 초과하거나 숫자가 아닙니다.\n다시 입력하세요");
 
-            n = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    break;
+                }
+            }
 
             for (int i=1; i<=n; i++)
             {
