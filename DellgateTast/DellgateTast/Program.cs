2bc94696bf3c65173a2295914042fd8cece12396
nezumi <khc_2007@naver.com>
2019-01-09 13:52:46 +0900

    sa

---
+++ DellgateTast/DellgateTast/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DellgateTast
+{
+    class Program
+    {
+        //delegate int SumDelegate(int i, int j);
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Func< int,int,int > s =  p.Sum;
+            Console.WriteLine(s(1, 2));
+        }
+         int Sum(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
---
+++ DllTest/DllTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllTest
+{
+    class Program
+    {
+        [DllImport("User32.Dll")]
+        public static extern int MessageBox(int h, string m, string c, int type);
+        static void Main(string[] args)
+        {
+            MessageBox(0, "Hello!", "In C#", 0); 
+        }
+    }
+}
