98701a3aaba421a0af10a0d981e13539ec3bb006
nezumi <khc_2007@naver.com>
2019-01-09 11:20:28 +0900

    wg

---
+++ defindTest/defindTest/Program.cs
@@ -0,0 +1,16 @@
+﻿#define SOUNDCARD
+using System;
+using System.Diagnostics;
+
+namespace defindTest
+{
+    class Program
+    {
+        static void print()   { Console.WriteLine("도레미");}
+
+        static void Main(string[] args)
+        {
+            print();
+        }
+    }
+}
