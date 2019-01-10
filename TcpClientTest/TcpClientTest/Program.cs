a671584f8d9a08b445d189107ce87da074bcc16c
nezumi <khc_2007@naver.com>
2019-01-10 11:39:18 +0900

    server

---
+++ TcpClientTest/TcpClientTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = null;
+            try
+            {
+                client = new TcpClient();
+                client.Connect("localhost", 5001);
+                NetworkStream stream = client.GetStream();
+                StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+                string dataToSend = Console.ReadLine();
+
+                while (true)
+                {
+                    writer.WriteLine(dataToSend);
+
+                    if (dataToSend.IndexOf("<EOF>") > -1) break;
+
+                    dataToSend = Console.ReadLine();
+
+
+                }
+
+
+
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                client.Close();
+            }
+
+        }
+    }
+}
